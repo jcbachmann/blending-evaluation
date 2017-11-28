335567219910f4152aab0bd13b4dee8735159f8e
Michael Cipold <mci@jcbachmann.de>
2017-11-28 19:57:07 +0100

    add reference bar to chevron layer evaluation graph

--- evaluate_chevron_layers.py
+++ evaluate_chevron_layers.py
@@ -6,9 +6,11 @@ import re
 import subprocess
 from multiprocessing import Pool
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib import gridspec
 from seaborn.palettes import color_palette
 
 from blendingsimulator import BlendingSimulator
@@ -41,22 +43,34 @@ def weighted_avg_and_std(values, weights):
 	return average, math.sqrt(variance)
 
 
-def get_results_for_file(layers, file, path):
-	df = pd.read_csv(path + '/' + file, delimiter='\t', index_col=None)
-
-	minvol = df['volume'].sum() / len(df.index)
-	larger = df.query('volume>=%f' % minvol)
-	smaller = df.query('volume<%f' % minvol)
-	lbound = min(larger['p1'].min(), np.average(smaller['p1'], weights=smaller['volume']))
-	mean, std = weighted_avg_and_std(df['p1'], weights=df['volume'])
+def get_results(meta, data: pd.DataFrame, c_meta='layers', c_weights: str = 'volume', c_values: str = 'p_1'):
+	minvol = 0.75 * data[c_weights].sum() / len(data.index)
+	larger = data.query('%s>=%f' % (c_weights, minvol))
+	lbound = larger[c_values].min()
+	mean, std = weighted_avg_and_std(data[c_values], weights=data[c_weights])
 	lstd = mean - std
 	ustd = mean + std
-	ubound = max(larger['p1'].max(), np.average(smaller['p1'], weights=smaller['volume']))
+	ubound = larger[c_values].max()
+
+	smaller = data.query('%s<%f' % (c_weights, minvol)).query('%s>0' % c_weights)
+	if len(smaller.index) > 0:
+		lbound = min(lbound, np.average(smaller[c_values], weights=smaller[c_weights]))
+		ubound = max(ubound, np.average(smaller[c_values], weights=smaller[c_weights]))
 
 	return pd.DataFrame(
-		data=[(layers, lbound, lstd, mean, ustd, ubound)],
-		columns=['layers', 'lbound', 'lstd', 'mean', 'ustd', 'ubound']
+		data=[(meta, lbound, lstd, mean, ustd, ubound)],
+		columns=[c_meta, 'lbound', 'lstd', 'mean', 'ustd', 'ubound']
 	)
+
+
+def get_results_for_file(layers, file, path):
+	df = pd.read_csv(path + '/' + file, delimiter='\t', index_col=None)
+	return get_results(meta=layers, data=df, c_values='p_1')
+
+
+def get_reference(file):
+	df = pd.read_csv(file, delimiter='\t', index_col=None)
+	return get_results(meta=0, data=df, c_values='p_1')
 
 
 def load_results_from_path(path):
@@ -72,22 +86,68 @@ def load_results_from_path(path):
 	return results.sort_values('layers')
 
 
-def plot_results(df):
-	ax = plt.gca()
+def plot_results(df_reference, df_layers):
+	fig = plt.figure(figsize=(20, 9))
+	fig.suptitle('Homogenization Sensitivity to Speed Change')
+	fig.subplots_adjust(top=0.94)
+	gs = gridspec.GridSpec(1, 2, width_ratios=[1, 15], wspace=0.05)
+	ax0 = plt.subplot(gs[0])
+	ax1 = plt.subplot(gs[1])
+
 	colors = color_palette(n_colors=1)
 
-	df = df.sort_values('layers')
-	x = df['layers'].values
+	y_range_raw = df_reference['ubound'][0] - df_reference['lbound'][0]
+	y_range_margin = 0.02 * y_range_raw
+	y_lim = [df_reference['lbound'][0] - y_range_margin, df_reference['ubound'][0] + y_range_margin]
 
-	ax.fill_between(x, df['lbound'], df['ubound'], facecolor=colors[0], alpha=0.2)
-	ax.fill_between(x, df['lstd'], df['ustd'], facecolor=colors[0], alpha=0.2)
-	ax.plot(x, df['mean'].values, color=colors[0], marker='', linestyle='-')
+	ax0.fill_between(
+		[0, 1],
+		[df_reference['lbound'][0], df_reference['lbound'][0]],
+		[df_reference['ubound'][0], df_reference['ubound'][0]],
+		facecolor=colors[0], alpha=0.3
+	)
+	ax0.fill_between(
+		[0, 1],
+		[df_reference['lstd'][0], df_reference['lstd'][0]],
+		[df_reference['ustd'][0], df_reference['ustd'][0]],
+		facecolor=colors[0], alpha=0.5
+	)
+	ax0.plot(
+		[0, 1],
+		[df_reference['mean'][0], df_reference['mean'][0]],
+		color=colors[0], linestyle='-'
+	)
 
-	ax.set_xlabel('layers')
-	ax.set_ylabel('quality')
-	ax.legend(loc=0)
+	ax0.set_xlim([0, 1])
+	ax0.set_ylim(y_lim)
+	ax0.set_xlabel('Raw Input')
+	ax0.set_ylabel('Material Output Quality')
+	ax0.legend(loc=0)
+	ax0.tick_params(
+		axis='x',
+		bottom='off',
+		labelbottom='off'
+	)
 
-	return ax
+	df_layers = df_layers.sort_values('layers')
+	x = df_layers['layers'].values
+
+	ax1.fill_between(x, df_layers['lbound'], df_layers['ubound'], facecolor=colors[0], alpha=0.3, label='Min/Max')
+	ax1.fill_between(x, df_layers['lstd'], df_layers['ustd'], facecolor=colors[0], alpha=0.5, label='Std')
+	ax1.plot(x, df_layers['mean'].values, color=colors[0], linestyle='-', label='Mean')
+
+	ax1.set_xlim([np.min(x), np.max(x)])
+	ax1.set_ylim(y_lim)
+	ax1.set_xlabel('Speed in Amount of Layers')
+	ax1.set_xscale('log')
+	ax1.set_xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+	ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+	ax1.legend(loc=0)
+	ax1.tick_params(
+		axis='y',
+		left='off',
+		labelleft='off'
+	)
 
 
 def main(args):
@@ -100,8 +160,9 @@ def main(args):
 			(args, '%s/reclaim-layers-%.4f.csv' % (args.path, layers), layers) for layers in np.linspace(1, 100, 397)
 		])
 
+	reference = get_reference(args.material)
 	results = load_results_from_path(args.path)
-	plot_results(results)
+	plot_results(reference, results)
 	plt.show()
 
 
