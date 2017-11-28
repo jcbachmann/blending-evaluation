0b2f9d7031c27424078089359a663eb9f0fc3afb
Michael Cipold <mci@jcbachmann.de>
2017-11-28 19:55:59 +0100

    unit test chevron stacker and update to new stacker implementation

--- chevron_stacker.py
+++ chevron_stacker.py
@@ -1,71 +1,31 @@
 #!/usr/bin/env python
 import argparse
-import signal
-import sys
 
 import pandas as pd
 
+from stacker import Stacker, Printer, read_material
 
-class Chevron:
-	finish = False
 
-	def __init__(self, args):
-		signal.signal(signal.SIGINT, self.signal_handler)
-		signal.signal(signal.SIGTERM, self.signal_handler)
-
-		# Blending bed parameters
-		self.length = args.length
-		self.depth = args.depth
-
-		self.material = pd.read_csv(args.material, delimiter='\t', index_col=None)
-		self.layers = args.layers
-
-	def run_linear(self):
-		# Stacker path parameters
-		min_pos = self.depth / 2
-		max_pos = self.length - self.depth / 2
-
-		# Total volume in cubic meters
-		t_total = self.material['timestamp'].max()
-		t_per_layer = t_total / self.layers
-		z = self.depth / 2
-
-		for _, row in self.material.iterrows():
-			if self.finish:
-				break
-
-			t = float(row['timestamp'])
-
-			# Position
-			p = (t % t_per_layer) / t_per_layer
-			if int(t / t_per_layer) % 2:
-				p = 1 - p
-			x = p * (max_pos - min_pos) + min_pos
-
-			sys.stdout.write('%f %f %f %f %f\n' % (t, x, z, row['volume'], row['p1']))
-
-		sys.stdout.close()
-
-	def run(self):
-		self.status('Starting generator')
-		try:
-			self.run_linear()
-		except IOError:
-			self.status('Stopping generator due to IOError')
-			self.finish = True
-		self.status('Generator stopped')
-
-	@staticmethod
-	def status(msg):
-		print("[generator] " + msg, file=sys.stderr)
-
-	def signal_handler(self, _signum, _frame):
-		self.status('Stopping generator')
-		self.finish = True
+def chevron_path(layers: float) -> pd.DataFrame:
+	path = [[float(f), float(f % 2)] for f in range(0, int(layers) + 1)]
+	if layers - int(layers) > 0:
+		# incomplete layer
+		path.append([layers, (layers - int(layers)) if int(layers) % 2 == 0 else (1 - (layers - int(layers)))])
+	return pd.DataFrame(data=path, columns=['part', 'path'])
 
 
 def main(args):
-	Chevron(args).run()
+	printer = Printer(header=False)
+	Stacker(
+		args.length,
+		args.depth,
+		status=printer.status
+	).run(
+		read_material(args.material),
+		chevron_path(args.layers),
+		callback=printer.out
+	)
+	printer.close()
 
 
 if __name__ == '__main__':
---
+++ tests/test_chevron_stacker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import unittest
+
+from chevron_stacker import chevron_path
+
+
+class TestChevronPath(unittest.TestCase):
+	def test_simple(self):
+		self.assertListEqual(list(chevron_path(1)['path'].values), [0, 1])
+		self.assertListEqual(list(chevron_path(2)['path'].values), [0, 1, 0])
+		self.assertListEqual(list(chevron_path(3)['path'].values), [0, 1, 0, 1])
+
+	def test_partial(self):
+		self.assertListEqual(list(chevron_path(0.5)['path'].values), [0, 0.5])
+		self.assertListEqual(list(chevron_path(1.25)['path'].values), [0, 1, 0.75])
+		self.assertListEqual(list(chevron_path(1.5)['path'].values), [0, 1, 0.5])
+		self.assertListEqual(list(chevron_path(2.25)['path'].values), [0, 1, 0, 0.25])
+		self.assertListEqual(list(chevron_path(2.75)['path'].values), [0, 1, 0, 0.75])
+		self.assertListEqual(list(chevron_path(3.25)['path'].values), [0, 1, 0, 1, 0.75])
+		self.assertListEqual(list(chevron_path(3.75)['path'].values), [0, 1, 0, 1, 0.25])
