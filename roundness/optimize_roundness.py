9337805a82a7cb9e94cfcb98c66749bea6da44ad
Michael Cipold <mci@jcbachmann.de>
2017-12-17 12:41:45 +0100

    move roundness related code into own module

--- roundness/optimize_roundness.py
+++ roundness/optimize_roundness.py
@@ -7,7 +7,7 @@ import pandas as pd
 from scipy.optimize import fminbound
 
 from ciglobal.ciplot import ciplot
-from roundness import execute_for_roundness
+from roundness.roundness import execute_for_roundness
 
 
 class OptEvaluator:
