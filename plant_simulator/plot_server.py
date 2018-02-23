f63cbd1daf138276b010fd6fd7c24a87aaa0deaf
Michael Cipold <mci@jcbachmann.de>
2018-02-23 17:10:13 +0100

    properly handle serving to multiple clients

--- plant_simulator/plot_server.py
+++ plant_simulator/plot_server.py
@@ -16,60 +16,61 @@ class PlotServer:
     PORT = 5001
 
     def __init__(self, columns):
-        self.doc = None
         self.data_callback = None
-        self.source = ColumnDataSource(pd.DataFrame(columns=columns).to_dict(orient='list'))
-
-        self.p = figure(
-            plot_height=500,
-            tools='xpan,xwheel_zoom,xbox_zoom,reset',
-            x_axis_type=None,
-            y_axis_label='Quality'
-        )
-        self.p.x_range.follow = 'end'
-        self.p.x_range.follow_interval = 7200000
-        self.p.x_range.range_padding = 0
-
-        palette = Category10[10]
-
-        c = 0
-        for column in columns:
-            if ' quality ' in column:
-                self.p.line(x='time', y=column, source=self.source, color=palette[c],
-                            legend=column.replace('quality ', ''))
-                c += 1
-        self.p.legend.location = 'bottom_left'
-        self.p.legend.orientation = 'horizontal'
-        self.p.legend.click_policy = 'mute'
-
-        self.p2 = figure(
-            plot_height=250,
-            x_range=self.p.x_range,
-            tools='xpan,xwheel_zoom,xbox_zoom,reset',
-            x_axis_type='datetime',
-            x_axis_label='Time',
-            y_axis_label='Tons per Hour'
-        )
-        c = 0
-        for column in columns:
-            if ' tph ' in column:
-                self.p2.line(x='time', y=column, source=self.source, color=palette[c])
-                c += 1
-
-    def update(self):
-        start = len(self.source.data['time'])
-        data = self.data_callback(start)
-        data['time'] = [t * 1000 for t in data['time']]
-        self.source.stream(data, 5760)
+        self.columns = columns
 
     def set_data_callback(self, data_callback):
         self.data_callback = data_callback
 
     def make_document(self, doc: Document):
         doc.title = 'Plant Simulation'
-        doc.add_root(gridplot([[self.p], [self.p2]], toolbar_location='left', plot_width=1000))
-        doc.add_periodic_callback(self.update, 500)
-        self.doc = doc
+
+        source = ColumnDataSource(pd.DataFrame(columns=self.columns).to_dict(orient='list'))
+
+        def update():
+            start = len(source.data['time'])
+            data = self.data_callback(start)
+            data['time'] = [t * 1000 for t in data['time']]
+            source.stream(data, 5760)
+
+        p = figure(
+            plot_height=500,
+            tools='xpan,xwheel_zoom,xbox_zoom,reset',
+            x_axis_type=None,
+            y_axis_label='Quality'
+        )
+        p.x_range.follow = 'end'
+        p.x_range.follow_interval = 7200000
+        p.x_range.range_padding = 0
+
+        palette = Category10[10]
+
+        i = 0
+        for col in self.columns:
+            if ' quality ' in col:
+                p.line(x='time', y=col, source=source, color=palette[i],
+                       legend=col.replace('quality ', ''))
+                i += 1
+        p.legend.location = 'bottom_left'
+        p.legend.orientation = 'horizontal'
+        p.legend.click_policy = 'mute'
+
+        p2 = figure(
+            plot_height=250,
+            x_range=p.x_range,
+            tools='xpan,xwheel_zoom,xbox_zoom,reset',
+            x_axis_type='datetime',
+            x_axis_label='Time',
+            y_axis_label='Tons per Hour'
+        )
+        i = 0
+        for col in self.columns:
+            if ' tph ' in col:
+                p2.line(x='time', y=col, source=source, color=palette[i])
+                i += 1
+
+        doc.add_root(gridplot([[p], [p2]], toolbar_location='left', plot_width=1000))
+        doc.add_periodic_callback(update, 500)
 
     def serve(self):
         print(f'Opening Bokeh application on http://localhost:{PlotServer.PORT}/')
