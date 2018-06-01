d577dc310b945217b4666e42850c86d2d2ed3744
Michael Cipold <mci@jcbachmann.de>
2018-06-01 14:28:29 +0200

    more generic preparation scripts to processing module

--- benchmark/evaluate_benchmark_data.py
+++ benchmark/evaluate_benchmark_data.py
@@ -9,6 +9,7 @@ from typing import Dict
 
 from benchmark import helpers
 from benchmark.benchmark import Benchmark
+from benchmark.processing import prepare_simulator, prepare_dst
 from benchmark.reference_meta import ReferenceMeta
 from benchmark.simulator_meta import SimulatorMeta
 from blending_simulator.material_deposition import MaterialMeta, DepositionMeta, MaterialDeposition
@@ -17,22 +18,6 @@ DATA_CSV = 'data.csv'
 RECLAIMED_MATERIAL_DIR = 'material'
 SIMULATOR_JSON = 'simulator.json'
 SIMULATOR_PARAMS_JSON = 'simulator_params.json'
-
-
-def prepare_dst(dst: str, dry_run: bool):
-    logging.debug(f'Preparing destination directory "{dst}"')
-    if os.path.exists(dst):
-        if os.path.isdir(dst):
-            if len(os.listdir(dst)) > 0:
-                raise IOError(f'Destination path "{dst}" is not empty')
-            else:
-                logging.debug(f'Destination path "{dst}" already exists and is empty')
-        else:
-            raise IOError(f'Destination path "{dst}" is not a directory')
-    else:
-        logging.debug(f'Creating destination path "{dst}"')
-        if not dry_run:
-            os.makedirs(dst)
 
 
 def process_reference(reference: ReferenceMeta, material: MaterialMeta, deposition: DepositionMeta, dst: str,
@@ -119,28 +104,6 @@ def process_data(benchmark: Benchmark, references: Dict[str, ReferenceMeta], dst
         )
 
     logging.info('Processing finished')
-
-
-def prepare_simulator(simulator_meta: SimulatorMeta):
-    logging.debug(f'Acquiring simulator type for "{simulator_meta.type}"')
-
-    # Acquire simulator type
-    sim_type = simulator_meta.get_type()
-
-    # Read simulator parameters
-    sim_params = simulator_meta.get_params()
-
-    sim_params_copy = sim_params.copy()
-    sim_params_copy['bed_size_x'] = 10
-    sim_params_copy['bed_size_z'] = 10
-
-    # Create demo simulator to test if params are all accepted
-    # If no exception occurs everything seems to be fine
-    logging.debug('Testing creation of simulator')
-    sim = sim_type(**sim_params_copy)
-
-    logging.debug('Testing deletion of simulator')
-    del sim
 
 
 def main(args: argparse.Namespace):
---
+++ benchmark/processing.py
@@ -0,0 +1,42 @@
+import logging
+import os
+
+from benchmark.simulator_meta import SimulatorMeta
+
+
+def prepare_simulator(simulator_meta: SimulatorMeta):
+    logging.debug(f'Acquiring simulator type for "{simulator_meta.type}"')
+
+    # Acquire simulator type
+    sim_type = simulator_meta.get_type()
+
+    # Read simulator parameters
+    sim_params = simulator_meta.get_params()
+
+    sim_params_copy = sim_params.copy()
+    sim_params_copy['bed_size_x'] = 10
+    sim_params_copy['bed_size_z'] = 10
+
+    # Create demo simulator to test if params are all accepted
+    # If no exception occurs everything seems to be fine
+    logging.debug('Testing creation of simulator')
+    sim = sim_type(**sim_params_copy)
+
+    logging.debug('Testing deletion of simulator')
+    del sim
+
+
+def prepare_dst(dst: str, dry_run: bool):
+    logging.debug(f'Preparing destination directory "{dst}"')
+    if os.path.exists(dst):
+        if os.path.isdir(dst):
+            if len(os.listdir(dst)) > 0:
+                raise IOError(f'Destination path "{dst}" is not empty')
+            else:
+                logging.debug(f'Destination path "{dst}" already exists and is empty')
+        else:
+            raise IOError(f'Destination path "{dst}" is not a directory')
+    else:
+        logging.debug(f'Creating destination path "{dst}"')
+        if not dry_run:
+            os.makedirs(dst)
