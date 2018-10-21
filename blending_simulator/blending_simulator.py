93d04336951d0ebfa1380776021fb640cd18d3fa
Michael Cipold <mci@jcbachmann.de>
2018-10-21 22:08:53 +0200

    convert nan to 0

--- blending_simulator/blending_simulator.py
+++ blending_simulator/blending_simulator.py
@@ -42,7 +42,7 @@ class BlendingSimulator:
         # reorganize reclaimed material into pandas DataFrame
         data_dict = pd.DataFrame(
             [row[2] for row in data_list], columns=material_deposition.material.get_parameter_columns()
-        ).to_dict(orient='list')
+        ).fillna(0).to_dict(orient='list')
         data_dict['x'] = [row[0] for row in data_list]
         data_dict['volume'] = [row[1] for row in data_list]
         data = pd.DataFrame(data_dict)
