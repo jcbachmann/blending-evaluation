16bb9cb7c92db6ae9365bc001098b44bda852dd5
Michael Cipold <mci@jcbachmann.de>
2018-10-22 21:42:52 +0200

    add optional evaluator observer

--- blending_optimization/multiprocess_evaluator.py
+++ blending_optimization/multiprocess_evaluator.py
@@ -11,10 +11,18 @@ def evaluate_solution(solution, problem):
     return solution
 
 
+class EvaluatorObserver:
+    def notify(self, solution_list: List[S]):
+        pass
+
+
 class MultiprocessEvaluator(Evaluator[S]):
-    def __init__(self, processes=None):
+    def __init__(self, processes=None, observer: EvaluatorObserver = None):
         self.pool = Pool(processes)
+        self.observer = observer
 
     def evaluate(self, solution_list: List[S], problem: Problem) -> List[S]:
         solution_list = self.pool.map(functools.partial(evaluate_solution, problem=problem), solution_list)
+        if self.observer is not None:
+            self.observer.notify(solution_list)
         return solution_list
