9e793c3bc691a82e8d5242207cb37cd9bb001cb2
Michael Cipold <mci@jcbachmann.de>
2018-10-31 12:47:06 +0100

    make package installable to allow easy command line execution

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Blending',
+    version='0.1',
+    description='Blending Evaluation',
+    author='Michael Cipold',
+    author_email='michael@cipold.de',
+    url='https://github.com/jcbachmann/blending-evaluation',
+    packages=[
+        'blending_optimization'
+    ],
+)
