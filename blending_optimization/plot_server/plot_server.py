8fa4711937ee82400239946ce175e32f4e71c55c
Michael Cipold <mci@jcbachmann.de>
2018-11-05 19:12:39 +0100

    auto format

--- blending_optimization/plot_server/plot_server.py
+++ blending_optimization/plot_server/plot_server.py
@@ -2,11 +2,13 @@ import logging
 import threading
 
 from typing import Callable
+
 logger = logging.getLogger(__name__)
 
 
 class PlotServer:
-    def __init__(self, all_callback: Callable, pop_callback: Callable, path_callback: Callable, port: int = 5001) -> None:
+    def __init__(self, all_callback: Callable, pop_callback: Callable, path_callback: Callable,
+                 port: int = 5001) -> None:
         self.all_callback = all_callback
         self.pop_callback = pop_callback
         self.path_callback = path_callback
