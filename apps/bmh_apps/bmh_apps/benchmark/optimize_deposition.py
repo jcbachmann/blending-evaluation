9f557304b6861a9a0462784d90a514d315c1279c
Michael Cipold <mci@jcbachmann.de>
2018-11-20 14:09:18 +0100

    add a minimal script to execute material deposition optimization

---
+++ apps/bmh_apps/bmh_apps/benchmark/optimize_deposition.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import argparse
+import math
+
+from bmh.benchmark.data import BenchmarkData
+from bmh.helpers.identifiers import get_identifier
+from bmh.optimization.optimization import optimize
+
+from bmh_apps.helpers.configure_logging import configure_logging
+
+
+def get_bed_size(volume: float, max_stockpile_height: float = 20.0):
+    max_volume = volume * 1.25
+    core_length = (max_volume - math.pi * math.pow(max_stockpile_height, 3) / 3) / math.pow(max_stockpile_height, 2)
+    bed_size_x = core_length + 2 * max_stockpile_height
+    bed_size_z = 2 * max_stockpile_height
+    return bed_size_x, bed_size_z
+
+
+def main(path: str, material_identifier: str):
+    configure_logging(verbose=False)
+    benchmark = BenchmarkData()
+    benchmark.read_base(path)
+    material_meta = benchmark.get_material_meta(material_identifier)
+    material = material_meta.get_material()
+    bed_size_x, bed_size_z = get_bed_size(volume=material_meta.volume)
+    optimize(
+        bed_size_x=bed_size_x,
+        bed_size_z=bed_size_z,
+        variables=30 + 1,
+        material=material.data,
+        parameter_columns=material.get_parameter_columns(),
+        population_size=250,
+        max_evaluations=25000
+    )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Optimize material deposition for a given material curve'
+    )
+    parser.add_argument('--path', default='.', help='Simulator benchmark path')
+    parser.add_argument('--material', type=str, default='generated_2Y45', help='Material curve identifier')
+    args = parser.parse_args()
+
+    main(path=args.path, material_identifier=get_identifier(args.material))
