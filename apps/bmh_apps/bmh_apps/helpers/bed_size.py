138608db8f399b78abc8479d1bcd1aff72e98695
Michael Cipold <mci@jcbachmann.de>
2018-12-04 12:52:32 +0100

    move volume factor to parameters

--- apps/bmh_apps/bmh_apps/helpers/bed_size.py
+++ apps/bmh_apps/bmh_apps/helpers/bed_size.py
@@ -2,8 +2,13 @@ import math
 from typing import Tuple
 
 
-def get_bed_size(volume: float, max_stockpile_height: float = 20.0) -> Tuple[float, float]:
-    max_volume = volume * 1.25
+def get_bed_size(
+        volume: float,
+        *,
+        max_stockpile_height: float = 20.0,
+        volume_factor: float = 1.25
+) -> Tuple[float, float]:
+    max_volume = volume * volume_factor
     core_length = (max_volume - math.pi * math.pow(max_stockpile_height, 3) / 3) / math.pow(max_stockpile_height, 2)
     bed_size_x = core_length + 2 * max_stockpile_height
     bed_size_z = 2 * max_stockpile_height
