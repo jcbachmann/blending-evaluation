85aee7676ca2de22c6027c595855a554019bd7ff
Michael Cipold <mci@jcbachmann.de>
2022-07-06 10:32:20 +0200

    Automatically derive an acceptable opacity based on the number of curves

--- apps/bmh_apps/bmh_apps/funvar/plot_var.py
+++ apps/bmh_apps/bmh_apps/funvar/plot_var.py
@@ -6,10 +6,21 @@ import plotly.express as px
 from .fun_var_results import FunVarResults
 
 
+def get_opacity(count: int) -> float:
+    offset = -5
+    factor = 6
+    power = 0.8
+    return 0.05 + min(factor * pow(1 / max(count - offset, 1), power), 0.95)
+
+
 def main(args: argparse.Namespace):
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
     results = FunVarResults.from_files(args.filename)
+    if results.len() == 0:
+        logging.warning('No results found')
+        return
+
     df = results.df.melt(
         id_vars=results.misc_columns,
         value_vars=results.var_columns,
@@ -18,7 +29,7 @@ def main(args: argparse.Namespace):
     )
 
     fig = px.line(df, x='var', y='value', line_group='run_individual', color='run')
-    fig.update_traces(opacity=0.05)
+    fig.update_traces(opacity=get_opacity(results.len()))
     fig.show()
 
 
