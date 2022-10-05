f5ddce5d7d85d70c6bc3c08ff16d1dc486813f27
Michael Cipold <mci@jcbachmann.de>
2022-10-05 21:20:02 +0200

    Improve fun plotting
    - Fix axis ranges for 2D and 3D plots
    - Add UUID and label to exported HTML
    - Add reference points to 2D and 3D plots
    - Add option to drop objective columns
    - Represent non-dominated filtering in file name
    - Minor code style improvements

--- apps/bmh_apps/bmh_apps/funvar/plot_fun.py
+++ apps/bmh_apps/bmh_apps/funvar/plot_fun.py
@@ -1,10 +1,13 @@
 import argparse
 import logging
+import os
 import time
+import uuid
 
 import plotly.express as px
+import plotly.graph_objects as go
 
-from bmh_apps.funvar import fun_var_math
+from .fun_var_math import filter_efficient_front
 from .fun_var_results import FunVarResults
 
 
@@ -17,26 +20,74 @@ def plot_fun_1d(results: FunVarResults):
     fig.show()
 
 
-def plot_fun_2d(results: FunVarResults):
+def plot_fun_2d(results: FunVarResults, label: str):
     fig = px.scatter(
         results.df,
         x=results.fun_columns[0],
         y=results.fun_columns[1],
-        color='run'
+        color='run',
+        title=label
     )
-    fig.write_html(f"plot-fun-2d-{int(time.time())}.html")
+
+    fig.update_xaxes(
+        range=[0, 1.5],
+        constrain="domain"
+    )
+    fig.update_yaxes(
+        range=[0, 1.5],
+        scaleanchor="x",
+        scaleratio=1,
+        constrain="domain"
+    )
+    fig.add_trace(go.Scatter(
+        x=[1],
+        y=[1],
+        mode='markers',
+        marker=dict(
+            size=20,
+            color='rgba(0, 0, 0, 1)',
+        ),
+        name='Reference Point'
+    ))
+    fig.write_html(os.path.join(
+        os.path.commonpath(results.df['file_path'].to_list()),
+        f"plot fun 2d {label} {str(uuid.uuid4())[:4]}.html"
+    ))
     fig.show()
 
 
-def plot_fun_3d(results: FunVarResults):
+def plot_fun_3d(results: FunVarResults, label: str):
     fig = px.scatter_3d(
         results.df,
         x=results.fun_columns[0],
         y=results.fun_columns[1],
         z=results.fun_columns[2],
-        color='run'
+        color='run',
+        title=label
     )
-    fig.write_html(f"plot-fun-3d-{int(time.time())}.html")
+    fig.update_layout(
+        scene=dict(
+            xaxis=dict(range=[0, 1.5]),
+            yaxis=dict(range=[0, 1.5]),
+            zaxis=dict(range=[0, 1.5]),
+        ),
+    )
+    fig.add_trace(go.Scatter3d(
+        x=[1],
+        y=[1],
+        z=[1],
+        mode='markers',
+        marker=dict(
+            size=20,
+            color='rgba(0, 0, 0, 1)',
+        ),
+        name='Reference Point'
+    ))
+    fig.update_layout(scene_aspectmode='cube')
+    fig.write_html(os.path.join(
+        os.path.commonpath(results.df['file_path'].to_list()),
+        f"plot fun 3d {label} {str(uuid.uuid4())[:4]}.html"
+    ))
     fig.show()
 
 
@@ -91,17 +142,28 @@ def plot_fun_5d(results: FunVarResults):
 def main(args: argparse.Namespace):
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
-    results = FunVarResults.from_files(args.filename)
+    results = FunVarResults.from_files(args.filename, fun_only=True)
+    label = results.label
+
+    additional_labels = []
+
+    if args.drop_columns:
+        results.drop_columns(args.drop_columns)
+        additional_labels.append(f"drop [{', '.join([c.replace('/', '_') for c in args.drop_columns])}]")
 
     if args.non_dominated:
-        results.df = fun_var_math.filter_efficient_front(results.df, results.fun_columns)
+        results.df = filter_efficient_front(results.df, results.fun_columns)
+        additional_labels.append("non-dominated")
+
+    if len(additional_labels) > 0:
+        label += " (" + ", ".join(additional_labels) + ")"
 
     if len(results.fun_columns) == 1:
         plot_fun_1d(results)
     elif len(results.fun_columns) == 2:
-        plot_fun_2d(results)
+        plot_fun_2d(results, label)
     elif len(results.fun_columns) == 3:
-        plot_fun_3d(results)
+        plot_fun_3d(results, label)
     elif len(results.fun_columns) == 4:
         plot_fun_4d(results)
     elif len(results.fun_columns) == 5:
@@ -115,6 +177,7 @@ def get_args():
     parser.add_argument('filename', type=str, nargs='+')
     parser.add_argument('--verbose', action='store_true', default=False, help='Enable verbose logging')
     parser.add_argument('--non-dominated', action='store_true', default=False, help='Show only non-dominated solutions')
+    parser.add_argument('--drop-columns', type=str, nargs='*', default=False, help='Columns/objectives to be dropped')
     return parser.parse_args()
 
 
